c6f45e3ec9956f7dc7ecc5a0d4163a0ab2e82d88
Safeat Mir <74701410+SafeatMir@users.noreply.github.com>
2022-11-28 23:12:23 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
-import Contact from "../components/contact";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
